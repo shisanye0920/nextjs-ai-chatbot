6a4fdf0c069aff7f639f4752b15be5172901392a
shisanye0920 <chepizbrick@gmail.com>
2025-11-21 11:39:16 +0800

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '50px', textAlign: 'center' }}>
+      <h1>🎨 艺术文本工作坊</h1>
+      <p>欢迎来到我的艺术文本工作坊！</p>
+      <p>这是我的 AI 聊天机器人网站！</p>
+    </div>
+  );
+}
